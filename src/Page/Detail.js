a9931f576c9e8995aa8dea2f9021e3c36eea89e7
orade <orademon@gmail.com>
2022-11-27 21:32:58 +0900

    DetailPage,Articleを取得してくるように修正

--- src/Page/Detail.js
+++ src/Page/Detail.js
@@ -1,10 +1,28 @@
+import { db } from "../firebase-config";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import YouTube from "react-youtube";
 import style from "../css/Youtube.module.css";
 import "../css/DetailPage.css";
+import { doc, DocumentSnapshot, getDoc } from "firebase/firestore";
 
 const Detail = (props) => {
+  const [article, setArticle] = useState({});
+  const { id } = useParams();
+  const articleDocumentationRef = doc(db, "articles", id);
+
+  useEffect(() => {
+    const getArticle = async () => {
+      const docSnap = await getDoc(articleDocumentationRef);
+      setArticle({ ...docSnap.data(), id: docSnap.id });
+      console.log("No such document!");
+    };
+    getArticle();
+    console.log(article);
+  }, []);
+
   return (
     <>
       <Row className="detailPageBackGround">
@@ -13,11 +31,11 @@ const Detail = (props) => {
           xl={{ span: 8, offset: 2 }}
           xs={{ span: 10, offset: 1 }}
         >
-          <p className="detailPageTitle">このリコンはテストです。</p>
+          <p className="detailPageTitle">{article.title}</p>
         </Col>
         <Col xl={{ span: 4, offset: 4 }} xs={{ span: 8, offset: 2 }}>
           <YouTube
-            videoId="ejIY4wfZhgg"
+            videoId={article.videoID}
             className={style.iframe}
             containerClassName={style.youtube}
           />
@@ -27,9 +45,7 @@ const Detail = (props) => {
           xl={{ span: 8, offset: 2 }}
           xs={{ span: 10, offset: 1 }}
         >
-          <p className="detailPageExplain">
-            このリコンはテストです。このリコンはテストです。このリコンはテストです。このリコンはテストです。このリコンはテストです。
-          </p>
+          <p className="detailPageExplain">{article.description}</p>
         </Col>
       </Row>
     </>
