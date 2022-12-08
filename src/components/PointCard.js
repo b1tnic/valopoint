b9e95e9c3cb65dd66f78fc408aee0be15d0a02bf
orade <orademon@gmail.com>
2022-12-09 01:38:21 +0900

    PointCard、レイアウト修正

--- src/components/PointCard.js
+++ src/components/PointCard.js
@@ -3,59 +3,50 @@ import { Clock } from "react-bootstrap-icons";
 import Button from "react-bootstrap/Button";
 
 const PointCard = (props) => {
-  let min = String(Math.floor(props.readTime / 60)) + "分";
-  let sec = String(props.readTime % 60) + "秒";
+  let min = String(Math.floor(props.readTime / 60));
+  let sec = String(props.readTime % 60);
 
   return (
     <>
       <div className="pointCard">
-        <div className="pointCardContent">
-          <div className="pointCardContentMap">
-            <img
-              src={require("../static/images/thumbnails/" +
-                props.thumbnail +
-                ".jpg")}
-              className="pointCardContentMapThumbnail"
-              alt={"Character"}
-            ></img>
-            <img
-              src={require("../static/images/sides/" + props.side + ".jpg")}
-              className="pointCardContentMapSide"
-              alt={"Side"}
-            ></img>
-            <img
-              src={require("../static/images/abilities/" +
-                props.character +
-                "/" +
-                props.ability +
-                ".jpg")}
-              className="pointCardContentMapAbility"
-              alt={"Ability"}
-            ></img>
-            <img
-              src={require("../static/images/characters/" +
-                props.character +
-                ".jpg")}
-              className="pointCardContentMapCharacter"
-              alt={"Character"}
-            ></img>
-            <p className="pointCardContentMapText">{props.title}</p>
+        <a href={"/Detail/" + props.id}>
+          <div className="pointCardContent">
+            <div className="pointCardContentMap">
+              <img
+                src={require("../static/images/thumbnails/" +
+                  props.thumbnail +
+                  ".jpg")}
+                className="pointCardContentMapThumbnail"
+                alt={"Character"}
+              ></img>
+              <img
+                src={require("../static/images/sides/" + props.side + ".jpg")}
+                className="pointCardContentMapSide"
+                alt={"Side"}
+              ></img>
+              <img
+                src={require("../static/images/abilities/" +
+                  props.character +
+                  "/" +
+                  props.ability +
+                  ".jpg")}
+                className="pointCardContentMapAbility"
+                alt={"Ability"}
+              ></img>
+              <img
+                src={require("../static/images/characters/" +
+                  props.character +
+                  ".jpg")}
+                className="pointCardContentMapCharacter"
+                alt={"Character"}
+              ></img>
+            </div>
           </div>
-        </div>
-        <div className="pointCardFooter">
-          <div className="pointCardFooterButton">
-            <Button
-              className="pointCardFooterDetail"
-              href={"/Detail/" + props.id}
-            >
-              ('ω')
-            </Button>
-          </div>
-          <div className="pointCardFooterClock">
-            <Clock color="royalblue" size={28} />
-          </div>
-          <p className="pointCardFooterClockText">{min + sec}</p>
-        </div>
+        </a>
+        <p className="pointCardContentMapClockText">
+          {min}:{sec}
+        </p>
+        <p className="pointCardContentTitle"> {props.title}</p>
       </div>
     </>
   );
